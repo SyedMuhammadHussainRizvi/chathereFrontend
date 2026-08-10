2a6995969349ad7ecba17ff874bca78c5e0ccb96
SyedMuhammadHussainRizvi <hussainrizwi45@gmail.com>
2026-08-10 17:09:10 +0500

    change socket lib from frontend

--- src/lib/socket.js
+++ src/lib/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:4000", {
+export const socket = io("https://ltm2wqrx-4000.inc1.devtunnels.ms", {
     autoConnect: false,
 });
